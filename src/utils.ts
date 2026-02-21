5e4e9991c1be87b87638775378dc8f4c0b666750
SergiiMost <s.v.mostovoyi@gmail.com>
2026-02-21 15:17:56 -0500

    Add comments

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,5 @@
 // A simple utility function to add two numbers.
 export const add = (a: number, b: number) => a + b;
 
+// A simple utility function to subtract two numbers.
 export const subtract = (a: number, b: number) => a - b;
